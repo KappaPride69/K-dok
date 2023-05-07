641bcb46f6080e9f1ad75d1ef0406d106f3bb5ac
KappaPride69 <kovacsricsi2001@gmail.com>
2023-05-07 18:30:27 +0200

    11 és 12 heti házik

---
+++ Python/week11/lotto.py
@@ -0,0 +1,19 @@
+import functools
+import itertools
+
+
+
+
+# Az összeg és a szorzat alapján meghatározzuk az ismeretlen számokat
+def main():
+    for combo in itertools.combinations(range(1, 46), 6):
+        if sum(combo) == 90 and functools.reduce(lambda x, y: x*y, combo) == 996300:
+            winner_numbers = combo
+            break
+
+    # Az eredmény kiírása
+    print("A nyerőszámok: {}".format(sorted(winner_numbers)))
+
+    # Futási idő ellenőrzés
+if __name__ == "__main__":
+    main()
